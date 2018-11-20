978230c1f534bf97220010cd1eeb279b8b517c08
komissarov <komissar.off.andrey@gmail.com>
2018-11-20 22:58:53 +0300

    Add creation work DB

--- miner/miner.py
+++ miner/miner.py
@@ -9,76 +9,66 @@ from .queries import *
 
 class Miner(Plugin):
 	def init(self):
-		self.n_i   = {} # name -> index
-		self.i_n   = {} # index -> name
-		self.count = 0  # count of tags
-		self._proc = 0.01 # procent of found tags
-
-		self.P.add_plugin(key="table",target=Table,autostart=False)
-		self.P.add_plugin(key="row",target=Row,autostart=False)
-
-		self.map = self.P.init_plugin(key="table")
-		self.cache_map = {}
-		self.cache_count = 0
-
+		pass
+		
 	#
 	# do with time count
 	#
 	def do(self,target,desc=None,*args,**kwargs):
 		_t = time.time()
 		try:
-			target(*args,**kwargs)
+			res = target(*args,**kwargs)
 		except Exception as e:
+			res = None
 			self.Error("{desc} - {e}".format(desc=str(target) if desc is None else desc,e=e))
 			self.Debug("{desc} - {e}".format(desc=str(target) if desc is None else desc,e=Trace()))
 		self.Notify("{desc} {t} sec".format(desc=str(target) if desc is None else desc,t=time.time()-_t))
-
-	#
-	# return index of tag
-	# of None if index >= count
-	#
-	def get_or_add_tag(self,tag):
-		if tag not in self.n_i:
-			l = len(self.n_i)
-			if l >= self.count:
-				return None
-			else:
-				self.n_i[tag] = l
-				self.i_n[l] = tag
-				if (l / self.count) >= self._proc:
-					self.Notify("found: %0.1f%%"%(100.0 * l / self.count))
-					self._proc += 0.01
-		else:
-			l = self.n_i[tag]
-		return l
-
-	#
-	# fill map and convert-tables
-	#
-	def load(self):
-		res = self.P.sql.select_all(SELECT_COUNT_OF_TAGS)
-		if res is None:	
-			self.Error("SQL is unreachable")
-			return
-		self.count = res[0][0]
-		for tag in self.P.sql.select(SELECT_MAPS):
-			tag = tag[0]
-			if self.get_or_add_tag(tag) is None:
-				break
-	
-	def smth(self):
-		for i in range(100):
-			for j in range(100):
-				self.Notify("smth: %2.0f.%2.0f%%"%(i,j))
-				if 0 < self.map[i][j] <= 1:
-					self.map[i][j] = 0
-		print("cache size: %s"%self.cache_count)
+		return None
 
 	def create_db(self):
-		self.P.sql.exec(???)
+		_c1 = self.P.sql.select_all(SELECT_COUNT_OF_TAGS)[0][0]
+		j = 0
+		for query in CREAT_WORK_TABLES:
+			boo , res = self.P.sql.execute(query, commit=True)
+			if not boo:
+				self.Error("create-work-tables {} :(".format(j))
+				return
+			else:
+				self.Notify("create-work-tables {} :)".format(j))
+			j += 1
+		_c2 = self.P.sql.select_all(SELECT_COUNT_OF_TAGS)[0][0]
+		self.Notify("Added {} rows to work.tag".format(_c2 - _c1))
+
+	def create_map(self):
+		ADD_PER_LOOP = 10**5
+		def update(az,cfg,dump=False,**kwargs):
+			if all(map(lambda x:x in kwargs,['src','dst','weight'])):
+				az.append("({src},{dst},{weight})".format(**kwargs))
+			if len(az) > ADD_PER_LOOP or dump:
+				boo , res = self.P.sql.execute(INSERT_MAP.format(values=",".join(az)),commit=True,unique_cursor=True)
+				cfg['i'] += 1
+				if 't' not in cfg:
+					cfg['t'] = time.time()
+				self.Notify("done: %2.4f%% ends in %s"%((cfg['i']/cfg['count']) , ( time.ctime(time.time() + (((time.time()-cfg['t'])*cfg['count']) / cfg['i'])))))
+				az = []
+				return boo
+			else:
+				return True
+		az = []
+		cfg = {
+			'i': 0,
+			'count':int(self.P.sql.select_all('''SELECT count(*)/{} FROM orb.tag_map;'''.format(ADD_PER_LOOP))[0][0]),
+		}
+		for row in self.P.sql.select(SELECT_MAPS):
+			src,dst,weight = row
+			if src > dst:
+				dst , src = src, dst
+			if not update(az=az,cfg=cfg,src=min(src,dst),dst=max(src,dst),weight=weight):
+				break
+		update(az=az,cfg=cfg,dump=True,src=min(src,dst),dst=max(src,dst),weight=weight)
 
 	def start(self):
-		self.do(self.create_db,desc="Copy database")
-		# self.do(self.load,desc="Load tags and indexs")
-		# self.do(self.smth,desc="Smth")
+		self.do(self.create_db,desc="Create work database")
+		self.do(self.create_map,desc="Create map")
 		self.P.stop()
+
--- miner/queries.py
+++ miner/queries.py
@@ -1,51 +1,37 @@
 #!/usr/bin/env python3
 
 SELECT_MAPS = '''
-SELECT distinct hashtag
-FROM orb.tag
-WHERE not isnull(rank_q)
-or not isnull(rank_p);
+SELECT t1.id,t2.id,m.weight
+FROM orb.tag_map m, 
+	work.tag t1, 
+	work.tag t2
+WHERE m.src_tag = t1.hashtag
+AND m.dst_tag = t2.hashtag
+AND 0 = (
+	SELECT count(*) as c
+	FROM work.map map
+	WHERE ( map.src = t1.id
+		AND map.dst = t2.id )
+	OR ( map.src = t2.id
+		AND map.dst = t1.id )
+)
+ORDER BY m.timestamp DESC
 ''' 
 
+INSERT_MAP = '''
+INSERT IGNORE INTO work.map 
+(src,dst,weight)
+VALUES
+{values}
+''' # format( values = '({src},{dst},{weight})' )
+
 SELECT_COUNT_OF_TAGS = '''
-SELECT count(distinct hashtag)
-FROM orb.tag
-WHERE not isnull(rank_q)
-or not isnull(rank_p);
+SELECT count(*)
+FROM work.tag;
 '''
 
-SELECT_WEIGHT = '''
-SELECT (sum(weight_1) + sum(weight_2))/2
-FROM 
-(
-	SELECT 0 as weight_1
-	UNION
-	(
-		SELECT weight as weight_1
-		FROM orb.tag_map
-		WHERE src_tag = '{src_tag}'
-		AND dst_tag = '{dst_tag}'
-		ORDER BY id DESC
-		LIMIT 1
-	)
-) t1,
-(
-	SELECT 0 as weight_2
-	UNION
-	(
-		SELECT weight as weight_2
-		FROM orb.tag_map
-		WHERE src_tag = '{dst_tag}'
-		AND dst_tag = '{src_tag}'
-		ORDER BY id DESC
-		LIMIT 1
-	)
-) t2
-''' # format( src_tag , dst_tag )
-
-
-# FIXME test
-CREAT_WORK_TABLE = '''
+CREAT_WORK_TABLES = [ 
+'''
 CREATE TABLE IF NOT EXISTS `work`.`tag` 
 (
 	`id` int UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -55,23 +41,30 @@ CREATE TABLE IF NOT EXISTS `work`.`tag`
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
+''',
+'''
 INSERT INTO work.tag
+(hashtag,rank)
 (
 	SELECT t1.hashtag, rank_q as rank
 	FROM
 	(
 		SELECT hashtag,max(id) as id
-		FROM orb.tag
-		WHERE not isnull(rank_p)
-		AND  not isnull(rank_q)
-		and rank_q > 0
+		FROM orb.tag t1 
+		WHERE hashtag not in (
+			SELECT hashtag
+			FROM work.tag
+		)
+		AND not isnull(rank_p)
+		AND not isnull(rank_q)
+		AND rank_q > 0
 		GROUP BY hashtag
 	) t1, 
 	orb.tag as t2
 	WHERE t1.id = t2.id
 );
-
+''',
+'''
 CREATE TABLE IF NOT EXISTS `work`.`map` 
 (
 	`src` int UNSIGNED NOT NULL,
@@ -81,50 +74,9 @@ CREATE TABLE IF NOT EXISTS `work`.`map`
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-SELECT t1.id as src,t2.id as dst,t3.weight
-FROM 
-(
-SELECT t1.hashtag, rank_q as rank
-	FROM
-	(
-		SELECT hashtag,max(id) as id
-		FROM orb.tag
-		WHERE not isnull(rank_p)
-		AND  not isnull(rank_q)
-		and rank_q > 0
-		GROUP BY hashtag
-	) t1, 
-	orb.tag as t2
-	WHERE t1.id = t2.id
-) t1,
-(
-SELECT t1.hashtag, rank_q as rank
-	FROM
-	(
-		SELECT hashtag,max(id) as id
-		FROM orb.tag
-		WHERE not isnull(rank_p)
-		AND  not isnull(rank_q)
-		and rank_q > 0
-		GROUP BY hashtag
-	) t1, 
-	orb.tag as t2
-	WHERE t1.id = t2.id
-) t2,
-(
-	SELECT t1.src_tag,t1.dst_tag,t2.weight
-	FROM
-	( 
-		SELECT src_tag,dst_tag,max(id) as id
-		FROM orb.tag_map tm
-		WHERE tm.weight > 0
-		GROUP BY src_tag,dst_tag
-	) t1,
-	orb.tag_map t2
-	WHERE t1.id = t2.id
-) t3
-WHERE t1.hashtag = t3.src_tag
-AND t2.hashtag = t3.dst_tag
-LIMIT 10;
 '''
+]
+
+SELECT_MAPS
+
+
