6b95006c8340913c648928e5dc53332a4233118f
riniuar <riniuar8@gmail.com>
2018-12-29 14:11:05 +0300

    smth done

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 
 import conf
 from kframe import Parent
-from kframe.plugins.sql   import SQL
+from kframe.plugins.sql import SQL
 from miner import Miner
 
+
 def main():
-	p = Parent(name="Mind Miner")
-	p.add_module(key="conf",target=conf)
-	p.add_plugin(key="sql",target=SQL,autostart=True,kwargs=conf.SQL)
-	p.add_plugin(key="miner",target=Miner,autostart=True,dependes=['sql','conf'])
-	p.init()
-	p.start()
+    p = Parent(name="Mind Miner")
+    p.add_module(key="conf", target=conf)
+    p.add_plugin(key="sql", target=SQL, autostart=True, kwargs=conf.SQL)
+    p.add_plugin(key="miner", target=Miner, autostart=True, dependes=['sql', 'conf'])
+    p.init()
+    p.start()
+
 
 if __name__ == '__main__':
-	main()
+    main()
